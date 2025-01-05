a82c253896dcec0667bc1490f3b01f7b5ebc0188
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2025-01-05 21:48:34 +0200

    [FIX] - Remove relations beetwen user and post

--- graphql/rootTypes/mutationType.js
+++ graphql/rootTypes/mutationType.js
@@ -3,7 +3,6 @@ import createUserMutation from '../mutations/createUserMutation.js';
 import updateUserMutation from '../mutations/updateUserMutation.js';
 import deleteUserMutation from '../mutations/deleteUserMutation.js';
 import loginMutation from '../mutations/loginMutation.js';
-import createPostMutation from '../mutations/createPostMutation.js';
 
 // Define the Query type
 const queryType = new graphql.GraphQLObjectType({
--- graphql/rootTypes/queryType.js
+++ graphql/rootTypes/queryType.js
@@ -1,7 +1,6 @@
-import { GraphQLObjectType } from 'graphql';
+import {GraphQLObjectType} from 'graphql';
 import userQuery from '../queries/userQuery.js';
 import usersQuery from '../queries/usersQuery.js';
-import postQuery from '../queries/postQuery.js';
 
 const queryType = new GraphQLObjectType({
     name: "Query",
