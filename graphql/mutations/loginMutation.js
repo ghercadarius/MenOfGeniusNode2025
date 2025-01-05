1bec6d89c59bd053a53e14c28970fc80c0f41030
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2025-01-05 23:48:27 +0200

    [IMPROVE] - Throw an error for BAD_USER_INPUT on loginMutation.js

--- graphql/mutations/loginMutation.js
+++ graphql/mutations/loginMutation.js
@@ -1,9 +1,10 @@
-import { JWT_SECRET } from '../../constants.js';
+import {JWT_SECRET} from '../../constants.js';
 import loginInputType from '../types/loginInputType.js';
 import loginResultType from '../types/loginResultType.js';
 import jwt from 'jsonwebtoken';
 import db from '../../models/index.js';
 import bcrypt from 'bcrypt';
+import {GraphQLError} from "graphql";
 
 const loginMutationResolver = async (_, args) => {
     const user = await db.User.findOne({
@@ -12,10 +13,12 @@ const loginMutationResolver = async (_, args) => {
         }
     });
 
-    if(!user) {
-        return {
-            token: null,
-        }
+    if (!user) {
+        throw new GraphQLError("Bad credentials", {
+            extensions: {
+                code: 'BAD_USER_INPUT',
+            },
+        });
     }
 
     const providedPassword = args.credentials.password;
@@ -23,16 +26,17 @@ const loginMutationResolver = async (_, args) => {
 
     const passwordIsValid = await bcrypt.compare(providedPassword, userPassword);
 
-    if(!passwordIsValid) {
+    if (!passwordIsValid) {
         console.log("Password is invalid");
-
-        return {
-            token: null,
-        }
+        throw new GraphQLError("Bad credentials", {
+            extensions: {
+                code: 'BAD_USER_INPUT',
+            },
+        });
     }
-    
-    const token = jwt.sign({ user_id: user.id }, JWT_SECRET);
-    
+
+    const token = jwt.sign({user_id: user.id}, JWT_SECRET);
+
     return {
         token,
     };
