4f9f484c116e7f08d5c8d86775d2ed7f546e3c6e
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2025-01-07 20:23:38 +0200

    Add new filter options - Filter products by category, min price, max price

--- core/repositories/productRepository.js
+++ core/repositories/productRepository.js
@@ -1,8 +1,33 @@
 import db from '../../models/index.js';
+import categoryType from "../../graphql/types/product/categoryType.js";
 
 class ProductRepository {
-    static async getAll() {
-        return await db.Product.findAll();
+    static async getAll(category, minPrice, maxPrice) {
+        const whereConditions = {};
+
+        if (category !== undefined)
+            whereConditions.category = {
+                [db.Sequelize.Op.like]: `%${category}%`,
+            };
+
+        if (minPrice !== undefined && maxPrice !== undefined)
+            whereConditions.price = {
+                [db.Sequelize.Op.between]: [minPrice, maxPrice]
+            };
+
+        if(minPrice !== undefined && maxPrice === undefined)
+            whereConditions.price = {
+                [db.Sequelize.Op.gte]: minPrice
+            };
+
+        if(minPrice === undefined && maxPrice !== undefined)
+            whereConditions.price = {
+                [db.Sequelize.Op.lte]: maxPrice
+            };
+
+        return await db.Product.findAll({
+            where: whereConditions,
+        });
     }
 
     static async getById(id) {
@@ -14,6 +39,17 @@ class ProductRepository {
     static save(product) {
         return db.Product.create(product);
     }
+
+    // static async getByCategory(category){
+    //     return await db.Product.findAll({
+    //         where: {
+    //             category: {
+    //                 [db.Sequelize.Op.like]: `%${category}%`,
+    //
+    //             }
+    //         }
+    //     });
+    // }
 }
 
 export default ProductRepository;
